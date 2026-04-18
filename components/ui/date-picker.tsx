dfdd7842ca462fadfe99af3776fc679677d91b74
YeChan Kim <az20058@naver.com>
2026-04-18 21:15:30 +0900

    Revert "fix: DatePicker 그리드만 스크롤되도록 변경하여 헤더 자연 고정"
    This reverts commit 8a394dbb8f57f7b4120d1ec16d39d10a67d0f894.

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -94,7 +94,7 @@ export function DatePicker({
           <DrawerTitle>날짜 선택</DrawerTitle>
         </DrawerHeader>
         <div
-          className="px-2 mobile-datepicker-scroll"
+          className="overflow-y-scroll px-2 mobile-datepicker-scroll"
           style={{
             maxHeight: 'calc(100dvh - 8rem)',
             paddingBottom: 'calc(env(safe-area-inset-bottom, 0px) + 2rem)',
